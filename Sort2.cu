f540933a771e8c3371efed54f86d3ca5a6ae6a61
majari <manjarityagi02@gmail.com>
2016-02-09 18:44:33 +0530

    Update Sort2.cu

--- Sort2.cu
+++ Sort2.cu
@@ -4,7 +4,7 @@
 int main()
 {
   const int N = 6;
-  int    keys[N] = {  1,   2,   1,   2,   4,   1};
+  int keys[N] = {  1,   2,   1,   2,   4,   1};
   int values[N] = {10,20,300,400,600,200};
 
   thrust::sort_by_key(keys, keys + N, values);
