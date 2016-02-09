491c913271c80fc7f4b66e87e9c1dbfade79c660
majari <manjarityagi02@gmail.com>
2016-02-09 19:38:01 +0530

    Update Sort2.cu

--- Sort2.cu
+++ Sort2.cu
@@ -1,15 +1,32 @@
-#include <thrust/sort.h>
+#include<thrust/sort.h>
 #include<stdio.h>
+#include<cuda.h>
+#include<thrust/device_ptr.h>
 
 int main()
 {
-  const int N = 6;
-  int keys[N] = {  1,   2,   1,   2,   4,   1};
-  int values[N] = {10,20,300,400,600,200};
+   const int N = 6;
+ 	 int keys_h[N] = {  1,   2,   1,   2,   4,   1};
+   int values_h[N] = {10,20,300,400,600,200};
+	 int *keys_d,*values_d; 
+	
+	size_t size = N * sizeof(int);
+ 
+	cudaMalloc((void **) &keys_d, size);
+	cudaMalloc((void **) &values_d, size);
+	 
+	cudaMemcpy(keys_d, keys_h, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(values_d, values_h, size, cudaMemcpyHostToDevice);
 
-  thrust::sort_by_key(keys, keys + N, values);
+  	thrust::sort_by_key(keys_h, keys_h + N, values_h);
+	
+	cudaMemcpy(keys_d, keys_h, size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(values_d, values_h, size, cudaMemcpyDeviceToHost);
 
- for(int i=0; i<N; i++)printf("%d %d \n",keys[i],values[i]);
-
-return 0;
+ 	for(int i=0; i<N; i++)printf("%d %d \n",keys_h[i],values_h[i]);
+	
+	free(keys_h); cudaFree(keys_d);
+	free(values_h); cudaFree(values_d);
+ 
+	 return 0;
 }
